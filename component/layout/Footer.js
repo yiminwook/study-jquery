d5e4d26ab203de639172f304db93e14726734334
yiminwook <grs0416@gmail.com>
2023-05-30 15:05:42 +0900

    Add: footer topButton

--- component/layout/Footer.js
+++ component/layout/Footer.js
@@ -1,4 +1,5 @@
-import Infomation from "../footer/Infomation";
+import Infomation from "@/component/footer/Infomation";
+import TopButton from "@/component/footer/TopButton";
 
 class Footer {
   $target = null;
@@ -7,6 +8,7 @@ class Footer {
     this.$target = $target;
     this.$footer = $("<footer class=footer></footer>");
     this.$infomation = new Infomation({ $target: this.$footer });
+    this.$topButton = new TopButton({ $target: this.$footer });
 
     this.$target.append(this.$footer);
   }
